155cde1b969e9049b823c4e91b5d6ef73e3cb9cf
Nestor36 <Personalmente36@gmail.com>
2023-11-21 23:41:30 -0500

    update

---
+++ readme.lua
@@ -0,0 +1,10 @@
+CSS	--https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css
+JS	--https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js
+
+container -- Nuestra clase .container predeterminada es un contenedor responsive de ancho fijo, lo que significa que su max-width cambia en cada punto de interrupción.
+
+container-fluid -- Usa .container-fluid para un contenedor de ancho completo, que abarque todo el ancho del viewport.
+
+mx-auto -- para centrar horizontalmente contenedor ->
+d-block -- con esto para bloquear el display-flex
+
