0c374176b5575c879f36bad395fdc4402854e084
Johnny Nestor Jair <Personalmente36@gmail.com>
2023-11-24 00:49:57 -0500

    Update readme.lua

--- readme.lua
+++ readme.lua
@@ -1,3 +1,60 @@
+<div align="center">
+  <img height="150" src="https://camo.githubusercontent.com/62da68eb62b1e5f175f7d1f0191dd89a653d7908feb22d37d4a0ab07365d6791/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4d3967624264396e6244724f5475314d71782f67697068792e676966"  />
+</div>
+
+###
+
+<div align="center">
+  <img src="https://img.shields.io/static/v1?message=LinkedIn&logo=linkedin&label=&color=0077B5&logoColor=white&labelColor=&style=for-the-badge" height="25" alt="linkedin logo"  />
+  <img src="https://img.shields.io/static/v1?message=Youtube&logo=youtube&label=&color=FF0000&logoColor=white&labelColor=&style=for-the-badge" height="25" alt="youtube logo"  />
+  <img src="https://img.shields.io/static/v1?message=Twitter&logo=twitter&label=&color=1DA1F2&logoColor=white&labelColor=&style=for-the-badge" height="25" alt="twitter logo"  />
+</div>
+
+###
+
+<div align="center">
+  <img src="https://visitor-badge.laobi.icu/badge?page_id=Nestor36.Nestor36&"  />
+</div>
+
+###
+
+<h1 align="center">Un Apasionado Desarrollador Backend de Perú 👋</h1>
+
+###
+
+<h3 align="left">👩‍💻  Anime-Manhwa</h3>
+<img src="https://i.imgur.com/tOcjPR1.jpg" />
+
+###
+
+<p align="left">Novedades: <br>- 🔭 Diseño intuitivo <br>- 👆 Secciones </p>
+
+###
+
+<h3 align="left">🛠 Lenguaje y Herramientas</h3>
+
+###
+
+<div align="left">
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" height="40" alt="html5 logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" height="40" alt="css3 logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firefox/firefox-original.svg" height="40" alt="firefox logo"  />
+  <img width="12" />
+  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg" height="40" alt="linux logo"  />
+</div>
+
+###
+
+<h3 align="left">🔥   Dev Nestor Ramos :</h3>
+<a href="https://github.com/nestor36" target="_blank">[Yo]</a>
+
+###
+
+
+
+
 CSS	--https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css
 JS	--https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js
 
